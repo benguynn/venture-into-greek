ffad831a844beaaf19eb2da1d4ecacdc9753121d
benguynn <benguynn@gmail.com>
2026-02-02 16:04:40 -0500

    Update service worker to cache additional assets

--- sw.js
+++ sw.js
@@ -1,6 +1,13 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('v1').then(c => c.addAll(['./', './index.html', './style.css', './script.js'])));
+const version = 'v1.1';
+const assets = [
+  './', './index.html', './style.css', './script.js', './vault.json',
+  './stone-texture.jpg', './paper-sounds-46696.mp3', './writingpensignaturepaper-102967.mp3'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(version).then(cache => cache.addAll(assets)));
 });
-self.addEventListener('fetch', (e) => {
+
+self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
